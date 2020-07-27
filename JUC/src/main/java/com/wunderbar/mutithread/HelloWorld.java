f6525952a3be97bb16a435d93c1fa2a1bdac7b07
Jawohl8756 <cestmagnifique@163.com>
2020-07-27 19:42:27 +0800

    to create an example .

---
+++ JUC/src/main/java/com/wunderbar/mutithread/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.wunderbar.mutithread;/**
+ *
+ */
+
+/**
+ * @author katze
+ * @date 2020/7/27 14:57
+ * @description
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Wenn du mich noch einmal anmachst,lasse ich dich feuern!");
+    }
+}
